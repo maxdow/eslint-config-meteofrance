42e5482f2c2831f5dfbd385b33a3a139212e9366
Maxime Warnier <marmax@gmail.com>
2016-06-14 12:35:58 +0200

    ajout de quoi valider les règles

---
+++ tests/app.js
@@ -0,0 +1,17 @@
+variableGlobale = "interdit";
+
+var console = window.console;
+
+if (variableGlobale) functionSale()
+
+
+  function functionSale(){
+
+   jappeleavantdeDeclarer();
+  var monScope = 123;
+    function jappeleavantdeDeclarer(){
+      console.log("hello",monScope);
+
+    }
+
+  }
